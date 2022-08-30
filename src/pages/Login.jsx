e4a6b3ebe149e1a8ee636103bfed8416e93982fe
Victor Mendes <victorutdsim7@gmail.com>
2022-08-30 16:58:28 -0300

    requisito 2 [completo]: cria tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,77 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Login extends Component {
+  state = {
+    username: '',
+    buttonIsDisabled: true,
+    isLoading: false,
+    shouldRedirect: false,
+  };
+
+  // componentDidUpdate(_prevProps, prevState) {
+  //   const { isLoading } = prevState;
+  //   if (isLoading) {
+  //     return <Redirect to="/search" />;
+  //   }
+  // }
+
+  changeHandle = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.buttonHandle();
+    });
+  };
+
+  buttonHandle = () => {
+    const { username } = this.state;
+    const MIN_LETTERS_TO_LOGIN = 3;
+    this.setState({
+      buttonIsDisabled: username.length < MIN_LETTERS_TO_LOGIN,
+    });
+  };
+
+  userHandle = async () => {
+    const { username } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name: username });
+    // <Redirect to="/search" />
+    this.setState({ shouldRedirect: true });
+  };
+
   render() {
+    const { buttonIsDisabled, isLoading, shouldRedirect } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/search" />;
+    }
+    if (isLoading) {
+      return <Loading />;
+    }
     return (
       <div data-testid="page-login">
         <h1>login page</h1>
+        <form>
+          <input
+            name="username"
+            data-testid="login-name-input"
+            type="text"
+            placeholder="nome"
+            onChange={ this.changeHandle }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ buttonIsDisabled }
+            onClick={ this.userHandle }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
