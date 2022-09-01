52b141c2801d6c36cd49c0c15a175e1b3cc75c22
Victor Mendes <victorutdsim7@gmail.com>
2022-09-01 09:09:57 -0300

    requisito 12 [completo]: página de favoritos

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 export default class Favorites extends Component {
   state = {
     isLoading: true,
+    favorites: [],
   };
 
+  async componentDidMount() {
+    const favorites = await getFavoriteSongs();
+    this.setState({
+      favorites: [...favorites],
+      isLoading: false,
+    });
+  }
+
+  async componentDidUpdate() {
+    const favorites = await getFavoriteSongs();
+    this.setState({
+      favorites: [...favorites],
+    });
+  }
+
   render() {
+    const { isLoading, favorites } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
         <h1>favorites page</h1>
+        {
+          isLoading
+            ? <Loading />
+            : favorites
+              .map((music) => <MusicCard key={ music.trackId } music={ music } />)
+        }
       </div>
     );
   }
