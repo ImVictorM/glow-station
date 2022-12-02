ee122d0d51290b60719744f1697cf186262986ef
Victor Mendes <victorutdsim7@gmail.com>
2022-12-02 12:13:08 -0300

    fix: change router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -11,19 +11,15 @@ import ProfileEdit from './pages/ProfileEdit';
 class App extends React.Component {
   render() {
     return (
-      <main>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/album/:id" component={ Album } />
-            <Route path="/profile/edit" component={ ProfileEdit } />
-            <Route path="/profile" component={ Profile } />
-            <Route path="/favorites" component={ Favorites } />
-            <Route path="/search" component={ Search } />
-            <Route exact path="/" component={ Login } />
-            <Route path="*" component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
-      </main>
+      <Switch>
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/search" component={ Search } />
+        <Route exact path="/" component={ Login } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     );
   }
 }
